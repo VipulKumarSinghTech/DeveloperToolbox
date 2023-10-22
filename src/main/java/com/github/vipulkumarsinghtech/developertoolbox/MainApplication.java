a171114eee93daed3c858bad4bd819a752b56c11
Vipul Kumar Singh <vipul24.9669@gmail.com>
2023-10-22 20:02:28 +0530

    Added Menubar

---
+++ src/main/java/com/github/vipulkumarsinghtech/developertoolbox/MainApplication.java
@@ -0,0 +1,24 @@
+package com.github.vipulkumarsinghtech.developertoolbox;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Menu Example");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/github/vipulkumarsinghtech/developertoolbox/MainController.java
@@ -0,0 +1,38 @@
+package com.github.vipulkumarsinghtech.developertoolbox;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    private VBox contentPane;
+
+    public void loadScene1() {
+        loadScene("scene1.fxml", "Scene 1");
+    }
+
+    public void loadScene2() {
+        loadScene("scene2.fxml", "Scene 2");
+    }
+
+    public void exitApplication() {
+        Stage stage = (Stage) contentPane.getScene().getWindow();
+        stage.close();
+    }
+
+    private void loadScene(String fxmlFileName, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent scene = loader.load();
+            Stage stage = (Stage) contentPane.getScene().getWindow();
+            stage.setTitle(title);
+            contentPane.getChildren().setAll(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
