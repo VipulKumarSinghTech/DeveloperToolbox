8ec679f6eff2e0a00c74b6b19cc2168e848bf1f1
Vipul Kumar Singh <vipul24.9669@gmail.com>
2023-11-19 20:27:56 +0530

    Added Decode and Copy actions

--- src/main/java/com/github/vipulkumarsinghtech/developertoolbox/Base64Controller.java
+++ src/main/java/com/github/vipulkumarsinghtech/developertoolbox/Base64Controller.java
@@ -3,21 +3,57 @@ package com.github.vipulkumarsinghtech.developertoolbox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Base64Controller {
 
     @FXML
-    private TextArea inputTextArea;
+    private TextArea encodeTextArea;
 
     @FXML
-    private Label resultLabel;
+    private TextArea decodeTextArea;
+
+    @FXML
+    private Label encodeResult;
+
+    @FXML
+    private Label decodeResult;
+
+    private static final String RESULT = "Result: ";
 
     @FXML
     private void encodeToBase64() {
-        String inputText = inputTextArea.getText();
+        String inputText = encodeTextArea.getText();
         String encodedText = Base64.getEncoder().encodeToString(inputText.getBytes());
-        resultLabel.setText("Base64 Encoded: " + encodedText);
+        encodeResult.setText(RESULT + encodedText);
+    }
+
+    @FXML
+    private void decodeToBase64() {
+        String inputText = decodeTextArea.getText();
+        byte[] decodedBytes = Base64.getDecoder().decode(inputText.getBytes());
+        String decodedText = new String(decodedBytes, StandardCharsets.UTF_8);
+        decodeResult.setText(RESULT + decodedText);
+    }
+
+    @FXML
+    private void copyEncodedResult() {
+        copyResult(encodeResult.getText().replace(RESULT, ""));
+    }
+
+    @FXML
+    private void copyDecodedResult() {
+        copyResult(decodeResult.getText().replace(RESULT, ""));
+    }
+
+    private void copyResult(String result) {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(result);
+        clipboard.setContent(content);
     }
 }
